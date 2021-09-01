86fce63baf619f4e89f16c83e6467c8223cb0b47
patsymejia <mejiarochapj@g.cofc.edu>
2021-09-01 15:39:34 -0400

    Complete HW1 :)

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,15 @@
+"""
+Name: Patsy Mejia-Rocha
+hello.py
+
+Problem: create a program that prints "hello, world!" to the screen.
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+def main():
+    print("hello, world!")
+
+
+main()
